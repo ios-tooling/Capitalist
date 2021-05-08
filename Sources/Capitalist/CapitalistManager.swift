5987296456e24fa0756edbcd2360d21a1a39ed46
Ben Gottlieb <ben@standalone.com>
2021-05-08 13:17:23 -0500

    added MobileProvisionFile

--- Sources/Capitalist/CapitalistManager.swift
+++ Sources/Capitalist/CapitalistManager.swift
@@ -386,3 +386,37 @@ extension CapitalistManager {
 		#endif
 	}
 }
+
+
+fileprivate class MobileProvisionFile {
+	fileprivate convenience init?(url: URL?) { self.init(data: url == nil ? nil : try? Data(contentsOf: url!)) }
+	
+	fileprivate var properties: NSDictionary!
+	
+	fileprivate static var `default`: MobileProvisionFile? = MobileProvisionFile(url: Bundle.main.url(forResource: "embedded", withExtension: "mobileprovision"))
+	fileprivate init?(data: Data?) {
+		guard let data = data else { return nil }
+		
+		guard let file = String(data: data, encoding: .ascii) else { return nil }
+		let scanner = Scanner(string: file)
+		if scanner.scanStringUpTo(string: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>") != nil, let contents = scanner.scanStringUpTo(string: "</plist>") {
+			let raw = contents.appending("</plist>")
+			self.properties = raw.propertyList() as? NSDictionary
+		}
+		
+		if self.properties == nil { return nil }
+	}
+}
+
+
+fileprivate extension Scanner {
+	 func scanStringUpTo(string: String) -> String? {
+		if #available(iOS 13.0, iOSApplicationExtension 13.0, watchOS 6.0, OSX 10.15, OSXApplicationExtension 10.15, *) {
+				return self.scanString(string)
+		  } else {
+				var result: NSString?
+				self.scanUpTo(string, into: &result)
+				return result as String?
+		  }
+	 }
+}
